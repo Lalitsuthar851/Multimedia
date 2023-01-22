116fd6c95174736bb969421c6c746e80712a766e
unknown <lsuthar@8511@gmail.com>
2023-01-23 00:01:29 +0530

    multi media project is uploaded

--- Multi_media/settings.py
+++ Multi_media/settings.py
@@ -123,9 +123,4 @@ STATIC_URL = 'weather/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER ="191260116061setiit@gmail.com"
-EMAIL_HOST_PASSWORD ="gtazscoqkbuwjnrr"
+
